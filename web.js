fd52fa3fab5e10c928cb8fd359b5f665e69b041b
Denny Kunker <wkunker@gmail.com>
2013-07-08 01:24:45 +0000

    Fixed problems using JSHint, thanks to lecture

--- web.js
+++ web.js
@@ -6,22 +6,25 @@ var indexPage;
 
 var indexLoadFail = function(e) {
   console.error('Problem loading index.html -- "' + e + '"');
-  process.exit(1)
-}
+  process.exit(1);
+};
 
-try {
-    indexPage = fs.readFileSync('index.html').toString()
+var loadIndex = function() {
+  try {
+    indexPage = fs.readFileSync('index.html').toString();
   } catch(e) {
-    indexLoadFail(e)
+    indexLoadFail(e);
   }
-}
+};
 
 app.get('/', function(request, response) {
   response.send(indexPage);
   if(indexPage === undefined) {
-    indexLoadFail('EXCEPTION NOT CAUGHT')
+    indexLoadFail('EXCEPTION NOT CAUGHT');
   }
   var port = process.env.PORT || 5000;
   app.listen(port, function() {
+    loadIndex();
     console.log("Listening on " + port);
+  });
 });
