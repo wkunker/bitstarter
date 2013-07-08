17ce860d01b8a38567231a2d421ef3a530474c88
Denny Kunker <wkunker@gmail.com>
2013-07-08 01:03:53 +0000

    modification of web.js for previous commit

--- web.js
+++ web.js
@@ -2,11 +2,29 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var indexPage;
+
+var indexLoadFail(e) {
+  console.error('Problem loading index.html -- "' + e + '"');
+  process.exit(1)
+}
+
+try {
+    indexPage = fs.readFileSync('index.html').toString()
+  } catch(e) {
+    indexLoadFail(e)
+  }
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+if(indexPage === undefined) {
+  indexLoadFail('EXCEPTION NOT CAUGHT')
+}
+
+  app.get('/', function(request, response) {
+
+  response.send(indexPage);
+
+  var port = process.env.PORT || 5000;
+  app.listen(port, function() {
+    console.log("Listening on " + port);
 });
